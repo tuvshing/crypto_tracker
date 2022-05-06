080b5443d28851939880085e6fab4ca0e346f476
tuvshing <tuvshincm@gmail.com>
2022-05-06 18:25:02 -0400

    small fixes
    touched up the table
    added pagination

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { CryptoList } from '../apiconf/api'
 import { CryptoState } from '../CoinContext'
 import { createTheme } from '@mui/material/styles'
-import { Container, LinearProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
+import { Container, tableCellClasses, LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom'
 import { makeStyles, ThemeProvider } from '@mui/styles';
 
@@ -13,25 +13,29 @@ const darkMode = createTheme({
       primary: {
         main: '#fff'
       },
-      type: 'dark',
-    },
-  })
-const useStyles = makeStyles(() => ({
-    row: {
-        backgroundColor: "#282a36",
-        cursor: 'pointer',
-        '&:hover': {
-            backgroundColor:'#44475a'
-        },
-        fontFamily: 'Open Sans',
+      type: 'dark'
     }
+  })
+// const useStyles = makeStyles(() => ({
+//     row: {
+//         backgroundColor: "#282a36",
+//         cursor: 'pointer',
+//         '&:hover': {
+//             backgroundColor:'#44475a'
+//         },
+//         fontFamily: 'Open Sans',
+//     },
+//     ul: {
+//         "& .MuiPaginationItem-root": {
+//           color: "#bd93f9",
+//           '&.Mui-selected': {
+//             background: '#44475a',
+//           },
+//         },
+//       }
+// }))
 
-
-}))
-
-function numberWithCommas(x){
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
 function currencyRounder (labelValue) {
     const temp = Math.abs(Number(labelValue))
@@ -47,14 +51,15 @@ function currencyRounder (labelValue) {
     : temp;
 }
 
-
 const CoinList = () => {
     const [cryptos, setCryptos] = useState([])
     const [load, setLoad] = useState(false)
     const {curr, symbol} = CryptoState()
     const [search, setSearch] = useState('')
+    const [page, setPage] = useState(1);
+
     const navigate = useNavigate()
-    const classes = useStyles()
+    // const classes = useStyles()
 
     // Fetch crypto data from api
     const fetch = async () => {
@@ -76,11 +81,7 @@ const CoinList = () => {
         ))
     }
 
-
-
-
-
-  return <ThemeProvider theme ={darkMode}>
+  return <ThemeProvider theme = {darkMode}>
       <Container style = {{textAlign: 'center'}}>
         <Typography
             variant = 'h4'
@@ -92,40 +93,50 @@ const CoinList = () => {
             backgroundColor: "#282a36",
             marginBottom:20, 
             width:'100%'}}
-            sx={{borderColor: 'white', 
+            sx={{borderRadius: 15,
+                "& .MuiInput-underline:before": {
+                    borderBottom: "2px solid white"
+                    },
                 input:{color: '#bd93f9'}}}
             InputLabelProps={{style: { color: 'white' }}}
             label='Search for a crypto currency...'
             variant='outlined' 
             onChange={(c) => setSearch(c.target.value)}
         />
-        <TableContainer>
+        <TableContainer style={{ borderRadius: 15}}>
             {load? (<LinearProgress style ={{backgroundColor: '#bd93f9'}}></LinearProgress>) : 
-                (<Table>
+                (<Table aria-label="table" sx={{
+                    [`& .${tableCellClasses.root}`]: {borderBottom: "none"} }}>
                     <TableHead style ={{backgroundColor: '#44475a'}}>
                         <TableRow>
                             {['Coin', 'Price', '24h Change', 'Market Cap'].map((head) =>(
                                 <TableCell style = {{
                                     color: '#bd93f9',
                                     fontWeight: '700',
-                                    fontFamily: 'Open Sans'
-                                }}
+                                    fontFamily: 'Open Sans'}}
                                 key={head}
                                 align={head === 'Coin' ? '': 'right'}
                                 >
                                 {head}
                                 </TableCell>
-                            ))
-                            }
+                            ))}
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {handleSearch().map((row) => {
+                    {handleSearch().slice((page - 1) * 20, (page - 1) * 20 + 20).map((row) => {
                             const p = row.price_change_percentage_24h > 0
                             return (
                                 <TableRow 
                                 onClick={() => navigate('/coins/${row.id}')}
-                                className={classes.row}
+                                // className={classes.row}
+                                sx={{
+                                    backgroundColor: "#282a36",
+                                    cursor: 'pointer',
+                                    '&:hover': {
+                                        backgroundColor:'#44475a'
+                                    },
+                                    fontFamily: 'Open Sans',
+                                }}
                                 key={row.name}>
                                     <TableCell 
                                     component='th'
@@ -135,16 +146,16 @@ const CoinList = () => {
                                         gap: 15,
                                     }}>
                                     <img 
-                                    src ={row?.image}
-                                    alt={row.name}
-                                    height='20'
-                                    style={{marginBottom: -2, marginRight: 6}}
+                                    src = {row?.image}
+                                    alt = {row.name}
+                                    height = '20'
+                                    style = {{marginBottom: -2, marginRight: 6}}
                                     />
                                     {/* <div style={{display:'flex', flexDirection: 'column'}}> */}
-                                        <span style ={{color:'white',textTransform: 'uppercase', fontSize: 20,  marginRight: 10}}>
+                                        <span style = {{color:'white',textTransform: 'uppercase', fontSize: 20,  marginRight: 10}}>
                                         {row.symbol}
                                         </span>
-                                        <span style ={{color:'darkgray'}}>
+                                        <span style = {{color:'darkgray'}}>
                                         {row.name}
                                         </span>
                                     {/* </div> */}
@@ -153,7 +164,8 @@ const CoinList = () => {
                                     style ={{color:'white',
                                     fontWeight: 700,}}>
                                     {symbol}{'$'}
-                                    {numberWithCommas(row.current_price.toFixed(2))}
+                                    {/* {numberWithCommas(row.current_price.toFixed(2))} */}
+                                    {row.current_price > 0.01 ? numberWithCommas(row.current_price.toFixed(2)) : Number(row.current_price.toPrecision(4))}
                                     </TableCell>
                                     <TableCell align = 'right'
                                     // determine if change is + or -
@@ -165,7 +177,7 @@ const CoinList = () => {
                                     {row.price_change_percentage_24h.toFixed(2)}%
                                     </TableCell>
                                     <TableCell align = 'right'
-                                    style ={{color: 'white',
+                                    style = {{color: 'white',
                                     fontWeight: 700,}}>
                                     {symbol}{"$"}
                                     {currencyRounder((row.market_cap))}
@@ -173,13 +185,30 @@ const CoinList = () => {
                                 </TableRow>
                             )
                         })}
-
-                        
                     </TableBody>
-
+                    
                 </Table>)
             }
         </TableContainer>
+        <Pagination
+          count={(handleSearch()?.length / 20).toFixed(0)}
+          style={{
+            padding: 20,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        //   classes={{ul: classes.ul}}
+        sx={{
+            "& .MuiPaginationItem-root": {
+          color: "#bd93f9",
+          '&.Mui-selected': { background: '#44475a'},
+        }}}
+          onChange={(_, val) => {
+            setPage(val);
+            window.scroll(0, 0);
+          }}
+        />
       </Container>
   </ThemeProvider>
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Banner from '../components/Banner/Banner'
 import CoinList from '../components/CoinList'
 
 const Homepage = () => {
   return <>
-    {/* <Banner /> */}
     <CoinList/>
   </>
 }
